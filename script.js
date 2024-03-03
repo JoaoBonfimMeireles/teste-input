a71833a2b8096f29cecb2fb607b3331c5570746c
JoaoBonfimMeireles <jpwmeireles44@gmail.com>
2024-03-03 03:05:51 -0300

    fiz com ajuda do chat

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
-let number = (Math.round(Math.random()*10))
-console.log(number)
+let number = Math.round(Math.random() * 10);
+console.log(number);
 let mensagem = "Você é um gênio!";
 let mensagem2 = "Você errou. Tente novamente.";
-let alterar = document.getElementById("text")
+let alterar = document.getElementById("text");
+let tentativa = [];
+let input
 
 
 function jogo() {
-    let input = Number(document.getElementById("input").value)
-    if(input == number) {
-        return alterar.innerHTML = mensagem
-    }else {
-        return alterar.innerHTML = mensagem2
+    let input = Number(document.getElementById("input").value);
+    tentativa.push(input);
+    if (input === number) {
+        alterar.innerHTML = `${mensagem} o número é ${number} e o numero de tentativas é ${tentativa.length}`;
+    } else {
+        alterar.innerHTML = mensagem2;
+        
     }
+    console.log(tentativa);
 }
 
-let button = document.getElementById("button")
-button.addEventListener("click", jogo)
+document.addEventListener("DOMContentLoaded", function() {
+    let inputField = document.getElementById("input")
+
+    inputField.addEventListener("click", function() {
+        this.value = ""
+    })
+})
+
+let button = document.getElementById("button");
+button.addEventListener("click", jogo);
